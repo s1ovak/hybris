8e4e04f3d5baa577981aed7d65df86480c3071f7
s1ovak <45860242+s1ovak@users.noreply.github.com>
2020-01-16 17:49:22 +0300

    Hybris working idea not

---
+++ bin/custom/concerttours/src/concerttours/daos/BandDAO.java
@@ -0,0 +1,26 @@
+package concerttours.daos;
+
+import concerttours.model.BandModel;
+
+import java.util.List;
+
+/**
+ * An interface for the Band DAO including various operations for retrieving persisted Band model objects
+ */
+public interface BandDAO
+{
+    /**
+     * Return a list of band models that are currently persisted. If none are found an empty list is returned.
+     *
+     * @return all the bands in the system
+     */
+    List<BandModel> findBands();
+    /**
+     * Finds all bands with given code. If none is found, an empty list will be returned.
+     *
+     * @param code
+     *           the code to search for bands
+     * @return All bands with the given code.
+     */
+    List<BandModel> findBandsByCode(String code);
+}
